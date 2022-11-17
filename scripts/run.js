4bf9607309e700cb43ad650fc1f075ab03cf1fc1
WagingBull <70824724+yugeorpheus@users.noreply.github.com>
2022-11-17 01:13:20 -0500

    add game characters

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,14 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Merchant", "Pirate", "Swashbuckler"],       // Names
+    ["https://i.imgur.com/R1hiu6L.jpg", // Images
+    "https://i.imgur.com/AufEo8J.jpg",
+    "https://i.imgur.com/RALlCwA.jpg"],
+    [200, 100, 150],                    // HP values
+    [35, 75, 50],                       // Attack damage values
+    [100, 20, 50]                       // Money values
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
